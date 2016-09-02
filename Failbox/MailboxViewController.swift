54f2e87c789ad63640e5e32579ebd917bcbd5748
Cam Hill <camkhill@gmail.com>
2016-09-02 00:07:22 -0400

    MVP done with menu swiping

--- Failbox/MailboxViewController.swift
+++ Failbox/MailboxViewController.swift
@@ -22,11 +22,19 @@ class MailboxViewController: UIViewController {
     @IBOutlet weak var archiveImage: UIImageView!
     @IBOutlet weak var listImage: UIImageView!
     @IBOutlet weak var rescheduleImage: UIImageView!
+    @IBOutlet weak var listDismissButton: UIButton!
+    @IBOutlet weak var rescheduleDismissButton: UIButton!
+    @IBOutlet var edgePanGestureRecognizer: UIScreenEdgePanGestureRecognizer!
+    @IBOutlet weak var menuImage: UIImageView!
     
     var messageOriginalCenter: CGPoint!
     let messageOffset: CGFloat = CGFloat(200)
     var checkOriginalCenter: CGPoint!
     var archiveOriginalCenter: CGPoint!
+    var feedOriginalCenter: CGPoint!
+    var menuOriginalCenter: CGPoint!
+    var edgeGesture: UIScreenEdgePanGestureRecognizer!
+    //let edgeGest = UIScreenEdgePanGestureRecognizer!(target: self(), action: "onEdgePan")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,14 +47,46 @@ class MailboxViewController: UIViewController {
         archiveImage.alpha = 1
         rescheduleImage.alpha = 0
         listImage.alpha = 0
+        listDismissButton.enabled = false
+        rescheduleDismissButton.enabled = false
 
-        // Do any additional setup after loading the view.
+        // Set up edge pan gesture recognizer
+        edgeGesture = UIScreenEdgePanGestureRecognizer(target: self, action: "onEdgePan:")
+        edgeGesture.edges = UIRectEdge.Left
+        view.addGestureRecognizer(edgeGesture)
+        
+        // Set up and disable pan gesture recognizer on the menu
+
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func onTapReschedule(sender: AnyObject) {
+        rescheduleDismissButton.enabled = false
+        UIView.animateWithDuration(0.2, animations: { 
+            self.rescheduleImage.alpha = 0
+        
+            }) { (Bool) in
+                self.onReturnToInbox()
+        }
+    }
+    
+    @IBAction func onTapList(sender: AnyObject) {
+        listDismissButton.enabled = false
+        UIView.animateWithDuration(0.2, animations: { 
+            self.listImage.alpha = 0
+            }) { (Bool) in
+                self.onReturnToInbox()
+        }
+        
+    }
+    
     
 
     @IBAction func onPanMessage(sender: UIPanGestureRecognizer) {
@@ -76,10 +116,12 @@ class MailboxViewController: UIViewController {
                 archiveImage.image = UIImage(named: "delete_icon")
                 archiveImage.center = CGPoint(x: archiveOriginalCenter.x + transform.x - 60, y: archiveOriginalCenter.y)
                 checkImage.alpha = 0
+                archiveImage.alpha = 1
             } else if 60 < transform.x && transform.x <= 260 {
                 self.topMessageView.backgroundColor = UIColor(red: 0.168, green: 0.908, blue: 0.399, alpha: 1)
                 archiveImage.center = CGPoint(x: archiveOriginalCenter.x + transform.x - 60, y: archiveOriginalCenter.y)
                 checkImage.alpha = 0
+                archiveImage.alpha = 1
             } else if -60 < transform.x && transform.x <= 60 {
                 self.topMessageView.backgroundColor = UIColor(red: 0.762, green: 0.762, blue: 0.762, alpha: 1)
                 
@@ -99,11 +141,13 @@ class MailboxViewController: UIViewController {
                 self.topMessageView.backgroundColor = UIColor(red: 0.994, green: 0.945, blue: 0.432, alpha: 1)
                 checkImage.center = CGPoint(x: checkOriginalCenter.x + transform.x + 60, y: checkOriginalCenter.y)
                 archiveImage.alpha = 0
+                checkImage.alpha = 1
             } else if transform.x <= -260 {
                 self.topMessageView.backgroundColor = UIColor(red: 0.645, green: 0.392, blue: 0, alpha: 1)
                 checkImage.image = UIImage(named: "list_icon")
                 checkImage.center = CGPoint(x: checkOriginalCenter.x+transform.x+60, y: checkOriginalCenter.y)
                 archiveImage.alpha = 0
+                checkImage.alpha = 1
             }
             
             
@@ -116,10 +160,7 @@ class MailboxViewController: UIViewController {
                     self.messageImage.center = CGPoint(x: self.messageOriginalCenter.x + 370, y: self.messageOriginalCenter.y)
                     }, completion: { (Bool) in
                         //Delete message
-                        //temp just return everything
-                        self.messageImage.center = self.messageOriginalCenter
-                        self.checkImage.center = self.checkOriginalCenter
-                        self.archiveImage.center = self.archiveOriginalCenter
+                        self.onReturnToInbox()
                         
                 })
 
@@ -129,11 +170,9 @@ class MailboxViewController: UIViewController {
                     self.archiveImage.alpha = 0
                     self.messageImage.center = CGPoint(x: self.messageOriginalCenter.x + 370, y: self.messageOriginalCenter.y)
                     }, completion: { (Bool) in
-                        //Delete message
-                        //temp just return everything
-                        self.messageImage.center = self.messageOriginalCenter
-                        self.checkImage.center = self.checkOriginalCenter
-                        self.archiveImage.center = self.archiveOriginalCenter
+                        //Slide up feed
+                        self.onReturnToInbox()
+                
                 })
             } else if -60 < transform.x && transform.x <= 60 {
                 //snap to center
@@ -149,12 +188,13 @@ class MailboxViewController: UIViewController {
                         //Show Alerts
                         UIView.animateWithDuration(0.2, animations: { 
                             self.rescheduleImage.alpha = 1
+                            self.rescheduleDismissButton.enabled = true
                         })
                         
-                        //temp just return everything
-                        self.messageImage.center = self.messageOriginalCenter
-                        self.checkImage.center = self.checkOriginalCenter
-                        self.archiveImage.center = self.archiveOriginalCenter
+                        
+                        //self.resetMessageParams()
+
+
                         
                 })
                 
@@ -167,13 +207,12 @@ class MailboxViewController: UIViewController {
                         //Show list
                         UIView.animateWithDuration(0.2, animations: { 
                             self.listImage.alpha = 1
+                            self.listDismissButton.enabled = true
                         })
                         
                         
-                        //temp just return everything
-                        self.messageImage.center = self.messageOriginalCenter
-                        self.checkImage.center = self.checkOriginalCenter
-                        self.archiveImage.center = self.archiveOriginalCenter
+                        //self.resetMessageParams()
+
                 })
             }
             
@@ -183,6 +222,116 @@ class MailboxViewController: UIViewController {
     }
     
     
+    //Simple function to reset the message position and images
+    func resetMessageParams() {
+        self.messageImage.center = self.messageOriginalCenter
+        self.checkImage.center = self.checkOriginalCenter
+        self.archiveImage.center = self.archiveOriginalCenter
+        archiveImage.image = UIImage(named: "archive_icon")
+        checkImage.image = UIImage(named: "later_icon")
+        self.feedImage.center = self.feedOriginalCenter
+        
+    }
     
+    
+    func onReturnToInbox() {
+        
+        let height = messageImage.frame.size.height
+        feedOriginalCenter = feedImage.center
+        UIView.animateWithDuration(0.3, animations: { 
+            self.feedImage.center = CGPoint(x: self.feedOriginalCenter.x, y: self.feedOriginalCenter.y - height)
+            
+            }) { (Bool) in
+                self.resetMessageParams()
+                // Should i animate this?
+                //UIView.animateWithDuration(0.5, animations: {
+                //    self.feedImage.center = self.feedOriginalCenter
+                //    self.resetMessageParams()
+                //})
+                
+        }
+        
+        
+    }
+    
+    
+    
+    @IBAction func onLeftEdgePan(sender: UIScreenEdgePanGestureRecognizer) {
+    }
+    
+    func onEdgePan(sender: UIScreenEdgePanGestureRecognizer) {
+        
+        let translation = sender.translationInView(view)
+        
+        if sender.state == UIGestureRecognizerState.Began{
+            menuOriginalCenter = menuImage.center
+            
+        } else if sender.state == UIGestureRecognizerState.Changed{
+            menuImage.center = CGPoint(x: menuOriginalCenter.x + translation.x, y: menuOriginalCenter.y)
+            
+        } else if sender.state == UIGestureRecognizerState.Ended{
+            
+            if translation.x > 160 {
+                sender.enabled = false
+                UIView.animateWithDuration(0.5, animations: { 
+                    self.menuImage.center.x = self.menuOriginalCenter.x + CGFloat(320)
+                    }, completion: { (Bool) in
+                        
+                        //build a pan gesture recognizer and attach it to the menu
+                        var menuPanGesture = UIPanGestureRecognizer(target: self, action: "onMenuPan:")
+                        self.menuImage.addGestureRecognizer(menuPanGesture)
+                        //enable pangesturerecognizer on the menu
+                })
+                
+            } else if translation.x <= 160 {
+                UIView.animateWithDuration(0.5, animations: {
+                    self.menuImage.center.x = self.menuOriginalCenter.x
+                    
+                })
+            }
+            
+        }
+    }
+    
+    func onMenuPan(sender: UIPanGestureRecognizer) {
+        //set up motion control
+        
+        let translation = sender.translationInView(view)
+        
+        if sender.state == UIGestureRecognizerState.Began{
+            menuOriginalCenter = menuImage.center
+        } else if sender.state == UIGestureRecognizerState.Changed{
+            
+            if translation.x >= 0 {
+                menuImage.center.x = menuOriginalCenter.x
+            } else if translation.x < 0 {
+                menuImage.center.x = menuOriginalCenter.x + translation.x
+            }
+            
+            
+        } else if sender.state == UIGestureRecognizerState.Ended{
+            if translation.x < -160 {
+                UIView.animateWithDuration(0.3, animations: {
+                    self.menuImage.center.x = self.menuOriginalCenter.x - 320
+                    }, completion: { (Bool) in
+                        self.edgeGesture.enabled = true
+                        
+                })
+                
+                
+            } else if translation.x >= -160 {
+                UIView.animateWithDuration(0.3, animations: {
+                    self.menuImage.center.x = self.menuOriginalCenter.x
+                    }, completion: { (Bool) in
+                        //Do nothing
+                })
+                
+            }
+            
+        }
+        
+        
+        
+    }
     
 }
